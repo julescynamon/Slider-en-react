feb02fefdd0a6de55232bb6fa73dfdee5afb3b27
cynamon jules <julescweb@mail.com>
2022-05-08 10:10:10 +0200

    on affiche les images et on met en place le prev et le next

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Slider from "./Components/Slider/Slider";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Slider />
+		</div>
+	);
 }
 
 export default App;
